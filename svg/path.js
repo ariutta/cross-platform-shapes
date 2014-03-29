10bc467e796bbd6b12ee60dffaaea9d49d3ff884
Anders Riutta <git@andersriutta.com>
2014-03-28 21:56:00 -0700

    gradient is kind of working

--- svg/path.js
+++ svg/path.js
@@ -15,6 +15,17 @@ crossPlatformShapes.svg.path = {
           x20 = p2[0] - p0[0], y20 = p2[1] - p0[1];
       return (x20 * x10 + y20 * y10) / (x10 * x10 + y10 * y10);
     }
+//http://stackoverflow.com/questions/9043805/test-if-two-lines-intersect-javascript-function
+function CCW(p1, p2, p3) {
+  a = p1[0]; b = p1[1]; 
+  c = p2[0]; d = p2[1];
+  e = p3[0]; f = p3[1];
+  return (f - b) * (c - a) > (d - b) * (e - a);
+}
+
+function isIntersect(p1, p2, p3, p4) {
+  return (CCW(p1, p3, p4) != CCW(p2, p3, p4)) && (CCW(p1, p2, p3) != CCW(p1, p2, p4));
+}
 
     var canvasPathCommandToSvgPathCommandMappings = {
       moveTo: 'M',
@@ -64,6 +75,8 @@ crossPlatformShapes.svg.path = {
       attributes.push({name: 'stroke', value: color});
     }
     if (!!markerStart || !!markerEnd) {
+      console.log('shapeName');
+      console.log(shapeName);
       markerStart = markerStart || 'lineStraight';
       markerEnd = markerEnd || 'lineStraight';
       svgPathGenerator.appendMarker(markerStart, 'start', color, function(markerId) {
@@ -81,13 +94,45 @@ crossPlatformShapes.svg.path = {
       var lastPoint = data.points[data.points.length - 1];
       var xDistance = lastPoint.x - firstPoint.x;
       var yDistance = lastPoint.y - firstPoint.y;
-      var distanceFromFirstPointToLastPoint = Math.pow(Math.pow((xDistance),2) + Math.pow((yDistance),2), 1/2);
+      var gradientVectorLength,
+        gradientVectorStart = {},
+        gradientVectorEnd = {},
+        offsetUnderMarker;
 
       if (data.points.length === 2) {
+        gradientVectorLength = Math.pow(Math.pow((xDistance),2) + Math.pow((yDistance),2), 1/2);          
         linearGradient.attr('x1', '0%')
         .attr('y1', '0%')
         .attr('x2', '100%')
         .attr('y2', '100%');
+
+        offsetUnderMarker = ((gradientVectorLength - 8)/gradientVectorLength) * 100;
+        console.log('gradientVectorLength');
+        console.log(gradientVectorLength);
+
+        linearGradient.append('stop')
+        .attr('offset', '0%')
+        .attr('stop-opacity', 0);
+
+        linearGradient.append('stop')
+        .attr('offset', (100 - offsetUnderMarker) + '%')
+        .attr('stop-opacity', 0);
+
+        linearGradient.append('stop')
+        .attr('offset', (100 - offsetUnderMarker) + '%')
+        .attr('stop-opacity', 1);
+
+        linearGradient.append('stop')
+        .attr('offset', (100 - offsetUnderMarker) + '%')
+        .attr('stop-color', color);
+
+        linearGradient.append('stop')
+        .attr('offset', offsetUnderMarker + '%')
+        .attr('stop-color', color);
+
+        linearGradient.append('stop')
+        .attr('offset', offsetUnderMarker + '%')
+        .attr('stop-opacity', 0);
       }
       else {
         // isMonotonic is looking at monotonicity in the coordinate system defined by the vector from first point to last point
@@ -96,60 +141,114 @@ crossPlatformShapes.svg.path = {
           maxY = -Infinity,
           minX = Infinity,
           minY = Infinity,
-          percentDistanceX,
-          percentDistanceY,
-          percentDistanceXWasGreaterThanPercentDistanceY,
-          percentDistanceXIsGreaterThanPercentDistanceY,
-          deviation,
-          maxDeviation = 0,
           maxDeviationXPoint,
-          maxDeviationYPoint;
+          maxDeviationYPoint,
+          previousPoint;
+        gradientVectorLength = 0;
+
+
+        if (data.points.length === 3) {
+          isMonotonic = true;
+        }
+
+        var p0 = [firstPoint.x, firstPoint.y],
+          p1 = [lastPoint.x, lastPoint.y];
+          var x10 = p1[0] - p0[0];
+          var y10 = p1[1] - p0[1];
+          var p3;
         data.points.forEach(function(point) {
+          var p2 = [point.x, point.y];
+          var t = pointLineSegmentParameter(p2, p0, p1);
+            p3 = [p0[0] + t * x10, p0[1] + t * y10];
+          currentGradientVectorLength = Math.pow(Math.pow((p3[0] - point.x),2) + Math.pow((p3[1] - point.y),2), 1/2);          
+          console.log('gradientVectorLength');
+          console.log(gradientVectorLength);
           maxX = Math.max(maxX, point.x);
           minX = Math.min(minX, point.x);
           maxY = Math.max(maxY, point.y);
           minY = Math.min(minY, point.y);
-          percentDistanceX = (point.x - firstPoint.x) / xDistance;
-          percentDistanceY = (point.y - firstPoint.y) / yDistance;
-          deviation = percentDistanceY - percentDistanceX;
-          if (Math.abs(deviation) > maxDeviation) {
-            maxDeviation = deviation;
+
+          svgSelection.append('path')
+          .attr('stroke', 'yellow')
+          .attr('stroke-width', 1)
+          .attr('d', 'M' + p3[0] + ',' + p3[1] + 'L' + point.x + ',' +  point.y);
+
+          if (currentGradientVectorLength > gradientVectorLength) {
+            console.log('currentGradientVectorLength > gradientVectorLength');
+            console.log(currentGradientVectorLength > gradientVectorLength);
+            gradientVectorLength = currentGradientVectorLength;
+            gradientVectorStart.x = p3[0];
+            gradientVectorStart.y = p3[1];
             maxDeviationXPoint = point.x;
             maxDeviationYPoint = point.y;
           }
-          percentDistanceXIsGreaterThanPercentDistanceY = (percentDistanceX > (point.y - firstPoint.y) / yDistance);
-          if (!!percentDistanceXWasGreaterThanPercentDistanceY && !isMonotonic) {
-            isMonotonic = (percentDistanceXIsGreaterThanPercentDistanceY === percentDistanceXWasGreaterThanPercentDistanceY);
+          if (!!previousPoint && !isMonotonic) {
+            isMonotonic = isIntersect([firstPoint.x, firstPoint.y], [lastPoint.x, lastPoint.y], [point.x, point.y], [previousPoint.x, previousPoint.y]);
+            console.log('isMonotonic');
+            console.log(isMonotonic);
           }
-          percentDistanceXWasGreaterThanPercentDistanceY = percentDistanceXIsGreaterThanPercentDistanceY;
+          previousPoint = point;
         });
         var bBoxWidth = maxX - minX;
         var bBoxHeight = maxY - minY;
+        console.log('isMonotonic: ' + isMonotonic);
         if (isMonotonic) {
+          gradientVectorLength = gradientVectorLength * 0.667;
+          linearGradient.attr('x1', (((gradientVectorStart.x - minX) / bBoxWidth) * 100) + '%')
+          .attr('y1', (((gradientVectorStart.y - minY) / bBoxHeight) * 100) + '%')
+          .attr('x2', (((maxDeviationXPoint - minX) / bBoxWidth) * 100) + '%')
+          .attr('y2', (((maxDeviationYPoint - minY) / bBoxHeight) * 100) + '%');
 
+          svgSelection.append('path')
+          .attr('stroke', 'pink')
+          .attr('stroke-width', 1)
+          .attr('stroke-dasharray', '5,5')
+          .attr('d', 'M' + gradientVectorStart.x + ',' + gradientVectorStart.y + 'L' + maxDeviationXPoint + ',' + maxDeviationYPoint);
 
-          var p0 = [firstPoint.x, firstPoint.y];
-          var p1 = [lastPoint.x, lastPoint.y];
-          var p2 = [maxDeviationXPoint, maxDeviationYPoint];
-          var t = pointLineSegmentParameter(p2, p0, p1),
-              x10 = p1[0] - p0[0],
-              y10 = p1[1] - p0[1],
-              p3 = [p0[0] + t * x10, p0[1] + t * y10];
-
-
-
-
-          linearGradient.attr('x1', (((p3[0] - minX) / bBoxWidth) * 100) + '%')
-          .attr('y1', (((p3[1] - minY) / bBoxHeight) * 100) + '%')
-          .attr('x2', ((1 - ((maxX - maxDeviationXPoint) / bBoxWidth)) * 100) + '%')
-          .attr('y2', ((1 - ((maxY - maxDeviationYPoint) / bBoxHeight)) * 100) + '%');
+          //*
+          svgSelection.append('path')
+          .attr('stroke', 'blue')
+          .attr('stroke-width', 1)
+          .attr('stroke-dasharray', '5,5')
+          .attr('d', 'M' + firstPoint.x + ',' + firstPoint.y + 'L' + lastPoint.x + ',' + lastPoint.y);
+          //*/
         }
         else {
+          gradientVectorLength = Math.pow(Math.pow((xDistance),2) + Math.pow((yDistance),2), 1/2);          
+
           linearGradient.attr('x1', (((firstPoint.x - minX) / bBoxWidth) * 100) + '%')
           .attr('y1', (((firstPoint.y - minY) / bBoxHeight) * 100) + '%')
           .attr('x2', ((1 - ((maxX - lastPoint.x) / bBoxWidth)) * 100) + '%')
           .attr('y2', ((1 - ((maxY - lastPoint.y) / bBoxHeight)) * 100) + '%');
         }
+
+        offsetUnderMarker = ((gradientVectorLength - 2 * 8)/gradientVectorLength) * 100;
+        console.log('gradientVectorLength');
+        console.log(gradientVectorLength);
+
+        linearGradient.append('stop')
+        .attr('offset', '0%')
+        .attr('stop-opacity', 0);
+
+        linearGradient.append('stop')
+        .attr('offset', '0%')
+        .attr('stop-color', color);
+
+        linearGradient.append('stop')
+        .attr('offset', '5%')
+        .attr('stop-opacity', 0.3);
+
+        linearGradient.append('stop')
+        .attr('offset', (100 - offsetUnderMarker) + '%')
+        .attr('stop-opacity', 0.3);
+
+        linearGradient.append('stop')
+        .attr('offset', (100 - offsetUnderMarker) + '%')
+        .attr('stop-opacity', 1);
+
+        linearGradient.append('stop')
+        .attr('offset', (100 - offsetUnderMarker) + '%')
+        .attr('stop-color', color);
       }
 
       /*
@@ -163,32 +262,7 @@ crossPlatformShapes.svg.path = {
       }
       //*/
 
-      var offsetUnderMarker = ((distanceFromFirstPointToLastPoint - 8)/distanceFromFirstPointToLastPoint) * 100;
 
-
-      linearGradient.append('stop')
-      .attr('offset', '0%')
-      .attr('stop-opacity', 0);
-
-      linearGradient.append('stop')
-      .attr('offset', (100 - offsetUnderMarker) + '%')
-      .attr('stop-opacity', 0);
-
-      linearGradient.append('stop')
-      .attr('offset', (100 - offsetUnderMarker) + '%')
-      .attr('stop-opacity', 1);
-
-      linearGradient.append('stop')
-      .attr('offset', (100 - offsetUnderMarker) + '%')
-      .attr('stop-color', color);
-
-      linearGradient.append('stop')
-      .attr('offset', offsetUnderMarker + '%')
-      .attr('stop-color', color);
-
-      linearGradient.append('stop')
-      .attr('offset', offsetUnderMarker + '%')
-      .attr('stop-opacity', 0);
 
       attributes.push({name: 'stroke', value: 'url(#' + 'gradient-for-' + data.id + ')'});
     }
@@ -314,4 +388,31 @@ crossPlatformShapes.svg.path = {
 	<line fill="none" stroke-width="1.8" x1="7" y1="0" x2="7" y2="20"></line>
 
 </g></marker>
+//*/
+
+/*
+  var myEdgeRenderingData = crossPlatformShapesInstance1.lineCurved({
+    id:'my-edge',
+    points:[{x:100,y:75},{x:1,y:190},{x:60,y:310},{x:100,y:325}],
+    color:'green',
+    markerEnd:'tBar'});
+  var myEdge = d3.select('svg').select('#viewport').append(myEdgeRenderingData.elementName)
+  myEdgeRenderingData.attributes.forEach(function(attribute) {
+    myEdge.attr(attribute.name, attribute.value);
+  });
+
+  var myNodeRenderingData = crossPlatformShapesInstance1.complex({
+    id:'my-node',
+    x:100,
+    y:300,
+    width:80,
+    height:50,
+    color:'brown',
+    backgroundColor:'white'});
+  var myNode = d3.select('svg').select('#viewport').append(myNodeRenderingData.elementName)
+  myNodeRenderingData.attributes.forEach(function(attribute) {
+    myNode.attr(attribute.name, attribute.value);
+  });
+d3.select('#my-edge').attr('fill', 'url(#gradient-for-my-edge)')
+d3.select('#edge2').attr('fill', 'url(#gradient-for-edge2)')
 //*/
