3ec3824ae23c0c1397043499b5eb7c0be903fcc5
khanspers <khanspers@gladstone.ucsf.edu>
2014-04-08 16:36:48 -0700

    updates commands for hexagon

--- path-calculator/hexagon.js
+++ path-calculator/hexagon.js
@@ -8,21 +8,22 @@ crossPlatformShapes.pathCalculator.hexagon = function(data){
 
   // TODO: the commands below are just a copy of the commands for rectangle.
   // We need to convert the commented out pathData below to the HTML5 Canvas path command format.
+  /*
   var pathData = [{command: 'moveTo', points: [x, y]},
     {command: 'lineTo', points: [(x + width), y]},
     {command: 'lineTo', points: [(x + width), (y + height)]},
     {command: 'lineTo', points: [(x), (y + height)]},
     {command: 'closePath', points: []}];
-
-    /*
-  var pathData = 'M' + (x + 0.25 * width) + ',' + y +
-    'l' + (0.5 * width) + ',' + 0 +
-    'l' + (0.25 * width) + ',' + (0.5 * height) +
-    'l' + (-0.25 * width) + ',' + (0.5 * height) +
-    'l' + (-0.5 * width) + ',' + 0 +
-    'l' + (-0.25 * width) + ',' + (-0.5 * height) +
-    'z';
-    //*/
+//*/
+    
+  var pathData = [{command: 'moveTo', points: [(x + 0.25 * width) , y]},
+    {command: 'lineTo', points: [(0.5 * width), 0]},
+    {command: 'lineTo', points: [(0.25 * width), (0.5 * height)]},
+    {command: 'lineTo', points: [(-0.25 * width), (0.5 * height)]},
+    {command: 'lineTo', points: [(-0.5 * width), 0]},
+    {command: 'lineTo', points: [(-0.25 * width), (-0.5 * height)]},
+    {command: 'closePath', points: []}];
+    
 
   return pathData;
 };
