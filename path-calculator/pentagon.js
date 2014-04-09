c6e4e70610dac47a9281307573bb2924cdc63489
khanspers <khanspers@gladstone.ucsf.edu>
2014-04-09 11:36:10 -0700

    adds new commands

--- path-calculator/pentagon.js
+++ path-calculator/pentagon.js
@@ -8,20 +8,20 @@ crossPlatformShapes.pathCalculator.pentagon = function(data){
 
   // TODO: the commands below are just a copy of the commands for rectangle.
   // We need to convert the commented out pathData below to the HTML5 Canvas path command format.
-  var pathData = [{command: 'moveTo', points: [x, y]},
+ /* var pathData = [{command: 'moveTo', points: [x, y]},
     {command: 'lineTo', points: [(x + width), y]},
     {command: 'lineTo', points: [(x + width), (y + height)]},
     {command: 'lineTo', points: [(x), (y + height)]},
-    {command: 'closePath', points: []}];
+    {command: 'closePath', points: []}]; //*/
 
-    /*
-  var pathData = 'M' + x + ',' + (y + 0.81*height) +
-    'l0,-' + 0.62*height +
-    'l' + 0.62*width + ',-' + 0.19*height +
-    'l' + 0.38*width+',' + 0.5*height +
-    'l-' + 0.38*width + ',' + 0.5*height +
-    'l-' + 0.62*width + ',-' + 0.19*height +
-    'z';
-    //*/
+   
+  var pathData = [{command: 'moveTo', points: [x, (y + 0.81*height)]},
+    {command: 'lineTo', points: [x, (y- 0.19*height)]},
+    {command: 'lineTo', points: [(x+ 0.62*width), y]},
+    {command: 'lineTo', points: [(x+ width), (y+ 0.5*height)]},
+    {command: 'lineTo', points: [(x-0.38*width), (y + height)]},
+    {command: 'closePath', points: []}];
+        /*{command: 'lineTo', points: [x, -' + 0.62*width + ',-' + 0.19*height +//*/
+        
   return pathData;
 };
