78a086b12fa592a77fd0c51baddf2e5ea76b4ec7
khanspers <khanspers@gladstone.ucsf.edu>
2014-04-09 11:08:05 -0700

    removes old code

--- path-calculator/hexagon.js
+++ path-calculator/hexagon.js
@@ -5,16 +5,6 @@ crossPlatformShapes.pathCalculator.hexagon = function(data){
     y = data.y,
     width = data.width,
     height = data.height;
-
-  // TODO: the commands below are just a copy of the commands for rectangle.
-  // We need to convert the commented out pathData below to the HTML5 Canvas path command format.
-  /*
-  var pathData = [{command: 'moveTo', points: [x, y]},
-    {command: 'lineTo', points: [(x + width), y]},
-    {command: 'lineTo', points: [(x + width), (y + height)]},
-    {command: 'lineTo', points: [(x), (y + height)]},
-    {command: 'closePath', points: []}];
-//*/
     
   var pathData = [{command: 'moveTo', points: [(x + 0.25*width) , y]},
     {command: 'lineTo', points: [(x + 0.75*width), y]},
