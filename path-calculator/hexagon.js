043984b8963f8fdf2428870bcde8905a2d627fe5
khanspers <khanspers@gladstone.ucsf.edu>
2014-04-09 11:02:53 -0700

    added last command

--- path-calculator/hexagon.js
+++ path-calculator/hexagon.js
@@ -16,11 +16,12 @@ crossPlatformShapes.pathCalculator.hexagon = function(data){
     {command: 'closePath', points: []}];
 //*/
     
-  var pathData = [{command: 'moveTo', points: [(x + 0.25 * width) , y]},
-    {command: 'lineTo', points: [(x + 0.75 * width), y]},
-    {command: 'lineTo', points: [(x + width), (y+ 0.5 * height)]},
-    {command: 'lineTo', points: [(x + 0.75 * width), (y + height)]},
-    {command: 'lineTo', points: [(x + 0.25 * width), (y + height)]},
+  var pathData = [{command: 'moveTo', points: [(x + 0.25*width) , y]},
+    {command: 'lineTo', points: [(x + 0.75*width), y]},
+    {command: 'lineTo', points: [(x + width), (y+ 0.5*height)]},
+    {command: 'lineTo', points: [(x + 0.75*width), (y + height)]},
+    {command: 'lineTo', points: [(x + 0.25*width), (y + height)]},
+    {command: 'lineTo', points: [x, (y+ 0.5*height)]},
     {command: 'closePath', points: []}];
     
 
