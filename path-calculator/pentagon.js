0a0923425e2c788f8e6aab1d34ea8994f7f3dc43
khanspers <khanspers@gladstone.ucsf.edu>
2014-04-09 11:46:25 -0700

    removes old code

--- path-calculator/pentagon.js
+++ path-calculator/pentagon.js
@@ -5,15 +5,6 @@ crossPlatformShapes.pathCalculator.pentagon = function(data){
     y = data.y,
     width = data.width,
     height = data.height;
-
-  // TODO: the commands below are just a copy of the commands for rectangle.
-  // We need to convert the commented out pathData below to the HTML5 Canvas path command format.
- /* var pathData = [{command: 'moveTo', points: [x, y]},
-    {command: 'lineTo', points: [(x + width), y]},
-    {command: 'lineTo', points: [(x + width), (y + height)]},
-    {command: 'lineTo', points: [(x), (y + height)]},
-    {command: 'closePath', points: []}]; //*/
-
    
   var pathData = [{command: 'moveTo', points: [x, (y + 0.81*height)]},
     {command: 'lineTo', points: [x, (y+ 0.19*height)]},
@@ -21,7 +12,6 @@ crossPlatformShapes.pathCalculator.pentagon = function(data){
     {command: 'lineTo', points: [(x+ width), (y+ 0.5*height)]},
     {command: 'lineTo', points: [(x+ 0.62*width), (y + height)]},
     {command: 'closePath', points: []}];
-        /*{command: 'lineTo', points: [x, -' + 0.62*width + ',-' + 0.19*height +//*/
-        
+
   return pathData;
 };
