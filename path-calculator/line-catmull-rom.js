b35496eb26c6e14a8c51147af351d0f2c327e9e1
Anders Riutta <git@andersriutta.com>
2014-07-03 21:41:30 -0700

    clean up code and fix bugs for curves and elbows

---
+++ path-calculator/line-catmull-rom.js
@@ -0,0 +1,82 @@
+// see also http://schepers.cc/svg/path/catmullrom2bezier.js
+crossPlatformShapes.pathCalculator.lineCurved = function(data){
+  'use strict';
+
+  var catmullRomPoints = data.points;
+  var pointCount = catmullRomPoints.length;
+
+  var firstCatmullRomPointWithNoOffset = catmullRomPoints[0];
+  var pathData = [{command: 'moveTo', points: [firstCatmullRomPointWithNoOffset.x, firstCatmullRomPointWithNoOffset.y]}];
+  /*
+
+  var markerStart = data.markerStart;
+  if (!!markerStart && firstCatmullRomPointWithNoOffset.anchor && typeof(firstCatmullRomPointWithNoOffset.anchor[2]) !== 'undefined' && typeof(firstCatmullRomPointWithNoOffset.anchor[3]) !== 'undefined') {
+    var firstCatmullRomPointWithOffset = catmullRomPoints[0];
+    var firstOffset;
+    var firstMarkerData = crossPlatformShapes.pathCalculator.markerData[markerStart];
+    if (!!firstMarkerData) {
+      firstOffset = firstMarkerData.markerHeight;
+    } else {
+      firstOffset = 12;
+    }
+    firstCatmullRomPointWithOffset.x = firstCatmullRomPointWithOffset.anchor[2] * firstOffset + firstCatmullRomPointWithOffset.x;
+    firstCatmullRomPointWithOffset.y = firstCatmullRomPointWithOffset.anchor[3] * firstOffset + firstCatmullRomPointWithOffset.y;
+    pathData.push({command: 'lineTo', points: [firstCatmullRomPointWithOffset.x, firstCatmullRomPointWithOffset.y]});
+  }
+
+  var lastCatmullRomPointWithNoOffset = catmullRomPoints[pointCount - 1];
+  var lastSegment = [];
+
+  var markerEnd = data.markerEnd;
+  if (!!markerEnd && lastCatmullRomPointWithNoOffset.anchor && typeof(lastCatmullRomPointWithNoOffset.anchor[2]) !== 'undefined' && typeof(lastCatmullRomPointWithNoOffset.anchor[3]) !== 'undefined') {
+    lastSegment.push({command: 'lineTo', points: [lastCatmullRomPointWithNoOffset.x, lastCatmullRomPointWithNoOffset.y]});
+
+    var lastCatmullRomPointWithOffset = catmullRomPoints[pointCount - 1];
+    var lastOffset;
+    var lastMarkerData = crossPlatformShapes.pathCalculator.markerData[markerEnd];
+    if (!!lastMarkerData) {
+      lastOffset = lastMarkerData.markerHeight;
+    } else {
+      lastOffset = 12;
+    }
+    lastCatmullRomPointWithOffset.x = lastCatmullRomPointWithOffset.anchor[2] * lastOffset + lastCatmullRomPointWithOffset.x;
+    lastCatmullRomPointWithOffset.y = lastCatmullRomPointWithOffset.anchor[3] * lastOffset + lastCatmullRomPointWithOffset.y;
+  }
+  //*/
+
+  for (var i = 0; pointCount - 1 > i; i++) {
+    var p = [];
+    if ( i === 0 ) {
+      p.push( {x: parseFloat(catmullRomPoints[ i ].x), y: parseFloat(catmullRomPoints[ i ].y)} );
+      p.push( {x: parseFloat(catmullRomPoints[ i ].x), y: parseFloat(catmullRomPoints[ i ].y)} );
+      p.push( {x: parseFloat(catmullRomPoints[ i + 1 ].x), y: parseFloat(catmullRomPoints[ i + 1 ].y)} );
+      p.push( {x: parseFloat(catmullRomPoints[ i + 2 ].x), y: parseFloat(catmullRomPoints[ i + 2 ].y)} );
+    } else if ( i === pointCount - 2 ) {
+      p.push( {x: parseFloat(catmullRomPoints[ i - 1 ].x), y: parseFloat(catmullRomPoints[ i - 1 ].y)} );
+      p.push( {x: parseFloat(catmullRomPoints[ i ].x), y: parseFloat(catmullRomPoints[ i ].y)} );
+      p.push( {x: parseFloat(catmullRomPoints[ i + 1 ].x), y: parseFloat(catmullRomPoints[ i + 1 ].y)} );
+      p.push( {x: parseFloat(catmullRomPoints[ i + 1 ].x), y: parseFloat(catmullRomPoints[ i + 1 ].y)} );
+    } else {
+      p.push( {x: parseFloat(catmullRomPoints[ i - 1 ].x), y: parseFloat(catmullRomPoints[ i - 1 ].y)} );
+      p.push( {x: parseFloat(catmullRomPoints[ i ].x), y: parseFloat(catmullRomPoints[ i ].y)} );
+      p.push( {x: parseFloat(catmullRomPoints[ i + 1 ].x), y: parseFloat(catmullRomPoints[ i + 1 ].y)} );
+      p.push( {x: parseFloat(catmullRomPoints[ i + 2 ].x), y: parseFloat(catmullRomPoints[ i + 2 ].y)} );
+    }
+
+    // Catmull-Rom to Cubic Bezier conversion matrix 
+    //    0       1       0       0
+    //  -1/6      1      1/6      0
+    //    0      1/6      1     -1/6
+    //    0       0       1       0
+
+    var bezierPoints = [];
+    bezierPoints.push( [ ((-p[0].x + 6 * p[1].x + p[2].x) / 6), ((-p[0].y + 6 * p[1].y + p[2].y) / 6) ] );
+    bezierPoints.push( [ ((p[1].x + 6 * p[2].x - p[3].x) / 6), ((p[1].y + 6 * p[2].y - p[3].y) / 6) ] );
+    bezierPoints.push( [ p[2].x, p[2].y ] );
+
+    pathData.push({command: 'bezierCurveTo', points: bezierPoints});
+  }
+
+  //pathData = pathData.concat(lastSegment);
+  return pathData;
+};
--- path-calculator/line-elbow.js
+++ path-calculator/line-elbow.js
@@ -15,26 +15,23 @@ crossPlatformShapes.pathCalculator.lineElbow = function(data){
   var pathData = [{command: 'moveTo', points: [firstPoint.x, firstPoint.y]}];
 
   var direction = [];
-  direction.push(firstPoint.anchor[2]);
-  direction.push(firstPoint.anchor[3]);
-
-  if (pointCount === 2) {
-    var x = firstPoint.x + Math.abs(firstPoint.anchor[2]) * (lastPoint.x - firstPoint.x);
-    var y = firstPoint.y + Math.abs(firstPoint.anchor[3]) * (lastPoint.y - firstPoint.y);
-    pathData.push({command: 'lineTo', points: [x, y]});
+  if (firstPoint.anchor) {
+    direction.push(firstPoint.anchor[2]);
+    direction.push(firstPoint.anchor[3]);
   } else {
-    points.forEach(function(point, index) {
-      if (index > 0 && index < pointCount - 1) {
-        var x0 = Math.abs(direction[0]) * (points[index].x - points[index - 1].x) + points[index - 1].x,
-          y0 = Math.abs(direction[1]) * (points[index].y - points[index - 1].y) + points[index - 1].y,
-          x1 = Math.abs(direction[1]) * (points[index + 1].x - points[index].x) + points[index].x,
-          y1 = Math.abs(direction[0]) * (points[index + 1].y - points[index].y) + points[index].y;
-        pathData.push({command: 'lineTo', points: [x0, y0]});
-        pathData.push({command: 'lineTo', points: [x1, y1]});
-        direction = changeDirection(direction);
-      }
-    });
+    // arbitrary
+    direction = [1, 0];
+    console.warn('No anchor specified for edge "' + data.id + '"');
   }
+
+  points.forEach(function(point, index) {
+    if (index > 0 && index < pointCount) {
+      var x0 = Math.abs(direction[0]) * (points[index].x - points[index - 1].x) + points[index - 1].x,
+        y0 = Math.abs(direction[1]) * (points[index].y - points[index - 1].y) + points[index - 1].y;
+      pathData.push({command: 'lineTo', points: [x0, y0]});
+      direction = changeDirection(direction);
+    }
+  });
 
   pathData.push({command: 'lineTo', points: [lastPoint.x, lastPoint.y]});
 
