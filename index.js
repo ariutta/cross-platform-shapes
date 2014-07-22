ab0c1ea46074398a9ed4174d6127f9ebeedbf29e
Anders Riutta <git@andersriutta.com>
2014-07-22 16:19:58 -0700

    Add back brace shape and update so it works with browserify better.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-module.exports = require('./lib/cross-platform-shapes');
+(function() {
+
+  // Baseline setup
+  // --------------
+
+  /** Used to determine if values are of the language type `Object` */
+  var objectTypes = {
+    'function': true,
+    'object': true
+  };
+
+  // Establish the root object, `window` in the browser, or `exports` on the server.
+  /** Used as a reference to the global object */
+  var root = (objectTypes[typeof window] && window) || this;
+
+  var crossPlatformShapes = require('./lib/cross-platform-shapes');
+  root.crossPlatformShapes = crossPlatformShapes;
+
+}.call(this));
+
