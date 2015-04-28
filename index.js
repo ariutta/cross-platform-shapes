33fda16138f67459dab826c6aa57c6efe9a4fecf
Anders Riutta <git@andersriutta.com>
2015-04-28 16:28:42 -0700

    Make it work better with browserify

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-(function() {
-
-  // Baseline setup
-  // --------------
-
-  /** Used to determine if values are of the language type `Object` */
-  var objectTypes = {
-    'function': true,
-    'object': true
-  };
-
-  // Establish the root object, `window` in the browser, or `exports` on the server.
-  /** Used as a reference to the global object */
-  var root = (objectTypes[typeof window] && window) || this;
+(function(window) {
 
   var crossPlatformShapes = require('./lib/cross-platform-shapes');
-  root.crossPlatformShapes = crossPlatformShapes;
 
-}.call(this));
+  if (!!module && !!module.exports) {
+    module.exports = crossPlatformShapes;
+  }
 
+  if (!!window) {
+    window.crossPlatformShapes = crossPlatformShapes;
+  }
+
+}(window));
